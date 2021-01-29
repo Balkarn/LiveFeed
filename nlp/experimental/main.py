0c6c2d775e1b3dc0a4c60848df3dc2136032ebb6
Adrian <adrianinc@users.noreply.github.com>
2021-01-29 02:18:59 +0000

    Added sentiment analysis from flair and textblob default models

--- nlp/experimental/main.py
+++ nlp/experimental/main.py
@@ -1,6 +1,11 @@
 from typing import Tuple
 #import moodparser
 import stanza
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
+import flair
+from hyperopt import hp
+from flair.hyperparameter.param_selection import SearchSpace, Parameter
 
 
 def bold_print(msg: str) -> str:
@@ -28,32 +33,61 @@ def batch_testing(texts: Tuple) -> None:
             "--------------------------------------------------------------------------------------------------------"))
 
 def stanza_test(texts: Tuple) -> None:
-    stanza.download('en')
-    nlp = stanza.Pipeline(lang='en', processors='tokenize,sentiment')
+    config = {
+        'lang':'en',
+        'processors':'tokenize,sentiment',
+        'use_device':'gpu'
+
+    }
+    #stanza.download('en')
+    nlp = stanza.Pipeline(**config)
     for text in texts:
         doc = nlp(text)
         for i, sentence in enumerate(doc.sentences):
-            print(sentence, sentence.sentiment)
+            print(text, sentence.sentiment)
 
+def textblob_test(texts: Tuple) -> None:
+    for text in texts:
+        testimonial = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+        print(text, testimonial.sentiment)
+
+
+def flair_test(texts: Tuple) -> None:
+    flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
+    for text in texts:
+        s = flair.data.Sentence(text)
+        flair_sentiment.predict(s)
+        total_sentiment = s.labels[0]
+        score = total_sentiment.score
+        score *= -1 if total_sentiment.value == "NEGATIVE" else 1
+        if score >= 0.6:
+            sentiment = "positive"
+            colour = 4
+        elif score >= -0.5:
+            sentiment = "neutral"
+            colour = 3
+        else:
+            sentiment = "negative"
+            colour = 1
+        print(colour_print(colour, sentiment + ", " + str(score) + ", " + text))
 
 if __name__ == "__main__":
     feedback1 = (
-        "This workshop is terrible",
-        "This talk is interesting",
+        "This workshop is terrible.",
+        "This talk is interesting.",
         "This guy is killing it!",
         "This company is bad ass.",
         "Please speed up. ",
-        "Zzzzz",
+        "Zzzzz.",
         "What are you doing!",
-        "Cool cool",
+        "Cool cool.",
         "Can you skip the safety briefing.",
-        "This is exhausting"
+        "This is exhausting."
     )
     feedback2 = (
         "I don't know who Apple typically sends over for these things, but I'm sure this guy "
         "isn't used to public speaking. ",
         "We've had enough!",
-        "Why are you still doing this to us!",
-        ""
+        "Why are you still doing this to us."
     )
-    stanza_test(feedback1)
+    flair_test(feedback1+feedback2)
