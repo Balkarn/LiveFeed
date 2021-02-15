82a175bee171c75529b289b06685fcfc43aa22ad
Aadil Khan <aadil.khan6425@gmail.com>
2021-02-15 16:40:57 +0000

    Separated Components
    - Create a new folder Screens to hold component screens
    - Move TemplateCreator code to Screens
    - Started on HostingEvent component

---
+++ livefeed-frontend/src/Screens/HostEventComponent.js
@@ -0,0 +1,15 @@
+import React, { useEffect/*, useState*/    } from "react";
+import { TextField, Button } from '@material-ui/core';
+import 'fontsource-roboto';
+
+const HostEventComponent = () => {
+
+    return (
+      <>
+        <p>Host Event</p>
+      </>
+    );
+  
+  }
+
+  export default HostEventComponent;
