e377bd5c6976da509e54bcbfa2042e697bc2216e
Balkarn <57596334+Balkarn@users.noreply.github.com>
2021-03-11 21:18:11 +0000

    started working on pie chart

--- livefeed-frontend/src/Screens/Meeting/Hostpart.js
+++ livefeed-frontend/src/Screens/Meeting/Hostpart.js
@@ -13,6 +13,61 @@ import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
 import SendIcon from '@material-ui/icons/Send';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+
+
+const DisplayAnalysis = ({question}) => {
+    const data = [
+        { name: 'Group A', value: 400 },
+        { name: 'Group B', value: 300 },
+        { name: 'Group C', value: 300 },
+        { name: 'Group D', value: 200 },
+    ];
+
+    const COLORS = ['#0088FE', '#00C49F', '#ff4040', '#f7911b'];
+
+    const customLabel = () => {
+        return (
+            "Label"
+        );
+    };
+
+
+    switch (question.questiontype) {
+
+        case 'Written Question':
+            return (
+                <Typography>Mood distribution and repeat analysis</Typography>
+            );
+        case 'Numerical Rating':
+            return (
+                <PieChart width={300} height={300}>
+                    <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                    <Pie data={data} dataKey="value" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                </PieChart>
+            );
+        case 'Multiple Choice':
+            return (
+                <PieChart width={300} height={300}>
+                    <Pie
+                        data={data}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={customLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[Math.max(0,index)]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+            );
+    }
+
+}
 
 const Hostpart = ({templateset}) => {
     const meetingtemplatset = templateset;
@@ -25,13 +80,14 @@ const Hostpart = ({templateset}) => {
                     <Divider />
                     {meetingtemplatset.map(question => (
                         <div>
-                    {/* <p key = {question.id}>{question.name}</p> */}
-                                <ListItem key={question.templateid}>
+                            <ListItem key={question.templateid}>
+                                <div>
                                     <Typography/> {question.templatename} <br/>
-                                    {question.questiontype} 
                                     <Typography/>
+                                    <DisplayAnalysis question={question}/>
+                                </div>
 
-                                </ListItem>
+                            </ListItem>
                             <Divider/>
                         </div>
                     
