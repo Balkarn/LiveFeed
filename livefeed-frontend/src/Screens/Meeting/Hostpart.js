db8c8240fde7c14276da7d75b1ae0f73d2c81914
Balkarn <57596334+Balkarn@users.noreply.github.com>
2021-03-11 20:48:41 +0000

    redoing meeting host screen for 1 template and showing graphs

--- livefeed-frontend/src/Screens/Meeting/Hostpart.js
+++ livefeed-frontend/src/Screens/Meeting/Hostpart.js
@@ -10,85 +10,37 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import IconButton from '@material-ui/core/IconButton';
 import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
 import SendIcon from '@material-ui/icons/Send';
 
 const Hostpart = ({templateset}) => {
-    const [modalisOpen, setModalisOpen] = React.useState(false);
-    const [graphisOpen,setGraphisOpen] = React.useState(false);
-    const [send,setSend] = React.useState(false);
-    const [templatesend,setTemplatesend] = React.useState([]);
     const meetingtemplatset = templateset;
-
-    const [senttemplates,setSenttemplates] = React.useState([]);
-    const [current,setCurrent] = React.useState([]);
 
     return (
         <div>
             <div className="list">
                 
                 <List>
+                    <Divider />
                     {meetingtemplatset.map(question => (
                         <div>
                     {/* <p key = {question.id}>{question.name}</p> */}
-                            <Divider /> 
                                 <ListItem key={question.templateid}>
-                                    <ListItemText
-                                    primary={question.templatename}
-                                    />
-                                    <ListItemSecondaryAction>
-                                    {senttemplates.includes(question) ? <IconButton>
-                                        <AssessmentOutlinedIcon color="primary" onClick = {()=>{setGraphisOpen(true);
-                                                                                  }}/>
-                                    </IconButton> : <IconButton>
-                                        <SendIcon color="primary" onClick = {()=>{setModalisOpen(true);
-                                                                                    setCurrent(question)
-                                                                                  }}/>
-                                    </IconButton>}
-                                    
-                                    </ListItemSecondaryAction>
+                                    <Typography/> {question.templatename} <br/>
+                                    {question.questiontype} 
+                                    <Typography/>
+
                                 </ListItem>
                             <Divider/>
                         </div>
                     
                     ))}
-                </List> 
-                <Dialog open={modalisOpen} 
-                        onClose={()=>setModalisOpen(false)} 
-                        aria-labelledby="form-dialog-title"
-                        fullWidth = 'xs'>
-                    <DialogTitle id="form-dialog-title">Send?</DialogTitle>
-                    <DialogContent>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={()=>{setModalisOpen(false);
-                                                setSend(true);
-                                                setTemplatesend(current);
-                                                senttemplates.push(current);
-                                        }} 
-                                color="primary">
-                            Send
-                        </Button>
-                        <Button onClick={()=>setModalisOpen(false)} color="primary">
-                            Cancel
-                        </Button>
-                    </DialogActions>
 
-                </Dialog>  
-                <Dialog open={graphisOpen} 
-                        onClose={()=>setGraphisOpen(false)} 
-                        aria-labelledby="form-dialog-title"
-                        fullWidth = 'xs'>
-                    <DialogTitle id="form-dialog-title">Graph</DialogTitle>
-                    <DialogContent>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={()=>setGraphisOpen(false)} color="primary">
-                            Cancel
-                        </Button>
-                    </DialogActions>
-                </Dialog>  
+                </List> 
+
             </div>
+
         </div>
 
     )
