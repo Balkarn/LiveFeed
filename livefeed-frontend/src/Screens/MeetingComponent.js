cbea04e62b045cd6674762974cfe545849c5ee23
Adrian <58747825+adrianinc@users.noreply.github.com>
2021-03-13 12:58:24 +0000

    Stop infinite server queries

--- livefeed-frontend/src/Screens/MeetingComponent.js
+++ livefeed-frontend/src/Screens/MeetingComponent.js
@@ -12,22 +12,16 @@ import axios from 'axios';
 
 const MeetingComponent = (props) => {
 
-    useEffect(() => {
-        document.body.style.backgroundColor = "#15bfff"
-    }, []); // Only run once
-    
+    var qs = require('qs');
+    const phpurl = "http://localhost:80/server/php/index.php";
+    let {id} = useParams();
     const [userid,setUsername] = React.useState('-1');
-    const [meetingid,setMeetingid] = React.useState();
+    const [meetingid,setMeetingid] = React.useState('-1');
     const [sessionname,setSessionname] = React.useState("test");
     const [sessiondate,setSessiondate] = React.useState("24-Feb-21");
     const [hostname,setHostname] = React.useState("James");
     const [templateid,setTemplateid] = React.useState([]);
-    var qs = require('qs');
-    const phpurl = "http://localhost:80/server/php/index.php";
-    let {id} = useParams();
-
     const [para,setPara] = React.useState(id.split('&'));// ['user_id','event_id']
-
     const [templateset,setTemplateset] = React.useState([
         { templateid : 1 , templatename : 'Question 1',questioncontent : 'content of Q1',questiontype : 'Written Question'},
         { templateid : 2 , templatename : 'Question 2',questioncontent : 'content of Q2',questiontype : 'Numerical Rating'},
@@ -39,44 +33,48 @@ const MeetingComponent = (props) => {
         {id : 3, name : 'attendee 3'},
     ]);
 
-    var data = {
-        function:"getmeetinginfo",
-        arguments:[Number(para[0]), Number(para[1])]
-      }
-    axios.post(phpurl, qs.stringify(data))
-        .then(res => {
-        
-        if (res.data.error) {
-            console.log(res.data.error)
-        }
-        if (res.data.result) {
-            setSessionname(res.data.result.MeetingName)
-            setSessiondate(res.data.result.StartTime)
-            setHostname(res.data.result.HostID)
-            setMeetingid(res.data.result.MeetingID)
-        }
-        })
-        .catch(err => console.log(err));
 
+    useEffect(() => {
+        document.body.style.backgroundColor = "#15bfff"
 
-    var data = {
-        function:"getmeetingtemplates",
-        arguments:[Number(meetingid)]
+        var data = {
+            function:"getmeetinginfo",
+            arguments:[Number(para[0]), Number(para[1])]
         }
-    axios.post(phpurl, qs.stringify(data))
-        .then(res => {
-        
-        if (res.data.error) {
-            console.log(res.data.error)
-        }
-        if (res.data.result) {
-            setTemplateid([res.data.result[0].TemplateID]);
-        }
-        })
-        .catch(err => console.log(err));
+        /*
+        axios.post(phpurl, qs.stringify(data))
+            .then(res => {
 
-    console.log(para)
-        
+                if (res.data.error) {
+                    console.log(res.data.error)
+                }
+                if (res.data.result) {
+                    console.log(res.data.result)
+                    setSessionname(res.data.result.MeetingName)
+                    setSessiondate(res.data.result.StartTime)
+                    setHostname(res.data.result.HostID)
+                    setMeetingid(res.data.result.MeetingID)
+                }
+            })
+            .catch(err => console.log(err));
+
+        axios.post(phpurl, qs.stringify({function:"getmeetingtemplates",arguments:['1']}))
+            .then(res => {
+
+                if (res.data.error) {
+                    console.log(res.data.error)
+                }
+                if (res.data.result) {
+                    console.log(res.data.result)
+                    setTemplateid([res.data.result[0].TemplateID]);
+                }
+            })
+            .catch(err => console.log(err));
+
+        console.log(para)*/
+    }, []); // Only run once
+    
+
     if(para[2] === 'host'){
         return (
     
