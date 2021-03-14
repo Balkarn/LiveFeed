6f9c24c5694657c6d4098b24cc660a9a81db31bb
Adrian <58747825+adrianinc@users.noreply.github.com>
2021-03-14 10:46:43 +0000

    Update livefeed-database.sql

--- server/database-schemas/livefeed-database.sql
+++ server/database-schemas/livefeed-database.sql
@@ -176,6 +176,24 @@ LOCK TABLES `mood_average` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `popular_feedback`
+--
+
+DROP TABLE IF EXISTS `popular_feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `popular_feedback` (
+  `ClusterID` int(11) NOT NULL AUTO_INCREMENT,
+  `ItemID` int(11) NOT NULL,
+  `MeetingID` int(11) NOT NULL,
+  `Phrase` varchar(144) NOT NULL,
+  PRIMARY KEY (`ClusterID`,`ItemID`),
+  KEY `popular_feedback-meetingid_idx` (`MeetingID`),
+  CONSTRAINT `popular_feedback-meetingid` FOREIGN KEY (`MeetingID`) REFERENCES `meetings` (`MeetingID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `mood_feedback`
 --
 
